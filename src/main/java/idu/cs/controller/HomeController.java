f951273f6641abbab837d68f707300f997363193
bumpy <bumpy1800@gmail.com>
2019-05-30 14:38:37 +0900

    ttthymeleaf

--- src/main/java/idu/cs/controller/HomeController.java
+++ src/main/java/idu/cs/controller/HomeController.java
@@ -2,6 +2,7 @@ package idu.cs.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
@@ -12,22 +13,49 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import idu.cs.domain.User;
 import idu.cs.exception.ResourceNotFoundException;
 import idu.cs.repository.UserRepository;
 
-@Controller
+@Controller // @Component, @Service, @Repository
+// Spring Framework에게 이 클래스로 부터 작성된 객체는 Controller 역할을 함을 알려줌
+// Spring 이 이 클래스로 부터 Bean 객체를 생성해서 등록할 수 있음
 public class HomeController {
 	@Autowired UserRepository userRepo; // Dependency Injection
 	@GetMapping("/")
 	public String home(Model model) { 
 		return "index";
 	}
-	@GetMapping("/user-reg-form")
+	@GetMapping("/user-login-form")
+	public String getLoginForm(Model model) {
+		return "login";
+	}
+	@PostMapping("/login")
+	public String loginUser(@Valid User user, HttpSession session) {
+		System.out.println("login process : ");
+		User sessionUser = userRepo.findByUserId(user.getUserId());
+		if(sessionUser == null) {
+			System.out.println("id error : ");
+			return "redirect:/user-login-form";
+		}
+		if(!sessionUser.getUserPw().equals(user.getUserPw())) {
+			System.out.println("pw error : ");
+			return "redirect:/user-login-form";
+		}
+		session.setAttribute("user", sessionUser);
+		return "redirect:/";
+	}
+	@GetMapping("/logout")
+	public String logoutUser(HttpSession session) {
+		session.removeAttribute("user");
+		// session.invalidate();
+		return "redirect:/";
+	}
+	
+	@GetMapping("/user-register-form")
 	public String getRegForm(Model model) {
-		return "form";
+		return "register";
 	}
 	@GetMapping("/users")
 	public String getAllUser(Model model) {
@@ -35,8 +63,11 @@ public class HomeController {
 		return "userlist";
 	}
 	@PostMapping("/users")
-	public String createUser(@Valid @RequestBody User user, Model model) {		
-		userRepo.save(user);
+	public String createUser(@Valid User user, Model model) {		
+		if(userRepo.save(user) != null)
+			System.out.println("Database 등록 성공");
+		else
+			System.out.println("Database 등록 실패");
 		model.addAttribute("users", userRepo.findAll());
 		return "redirect:/users";
 	}
--- src/main/java/idu/cs/domain/User.java
+++ src/main/java/idu/cs/domain/User.java
@@ -1,5 +1,6 @@
 package idu.cs.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,15 +8,28 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "user")
+@Table(name = "user_table")
 public class User {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id; 
-	// database에서 sequence number, primary key 역할
+	// database에서 sequence number, auto increment => primary key 역할
 	
+	@Column(nullable=false, length=20, unique=true)
+	private String userId;
+	private String userPw;
 	private String name;
 	private String company;
+	
+	public User() {}
+	
+	public User(String userId, String userPw, String name, String company) {
+		super();
+		this.userId = userId;
+		this.userPw = userPw;
+		this.name = name;
+		this.company = company;
+	}
 	
 	public Long getId() {
 		return id;
@@ -23,6 +37,20 @@ public class User {
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPw() {
+		return userPw;
+	}
+	public void setUserPw(String userPw) {
+		this.userPw = userPw;
+	}
+	
 	public String getName() {
 		return name;
 	}
@@ -35,7 +63,7 @@ public class User {
 	public void setCompany(String company) {
 		this.company = company;
 	}
-	
+
 	
 	
 }
