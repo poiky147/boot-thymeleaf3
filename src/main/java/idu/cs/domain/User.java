020edb174d21047f702c568a4ef734cfb1e02423
poiky147 <poiky147@gmail.com>
2019-06-04 08:54:26 +0900

    ssh thymeleaf

---
+++ src/main/java/idu/cs/domain/User.java
@@ -0,0 +1,69 @@
+package idu.cs.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user_table")
+public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id; 
+	// database에서 sequence number, auto increment => primary key 역할
+	
+	@Column(nullable=false, length=20, unique=true)
+	private String userId;
+	private String userPw;
+	private String name;
+	private String company;
+	
+	public User() {}
+	
+	public User(String userId, String userPw, String name, String company) {
+		super();
+		this.userId = userId;
+		this.userPw = userPw;
+		this.name = name;
+		this.company = company;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPw() {
+		return userPw;
+	}
+	public void setUserPw(String userPw) {
+		this.userPw = userPw;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	
+	
+}
---
+++ src/main/java/idu/cs/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package idu.cs.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import idu.cs.domain.User;
+
+@Repository
+public interface UserRepository 
+	extends JpaRepository<User, Long> {
+	List<User> findByName(String name);
+	List<User> findByCompany(String company);
+	User findByUserId(String userId); // ByUserId == where userid=''
+}
